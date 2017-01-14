e24290b40f8f18ee4ff8761f4eab1ec0122e3df3
Balin <sinnott.balin@gmail.com>
2017-01-14 12:06:51 -0500

    Initial Commit

---
+++ BitcoinFreelancing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BitcoinFreelancing
+//
+//  Created by Balin Sinnott on 1/14/17.
+//  Copyright © 2017 Balin Sinnott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
